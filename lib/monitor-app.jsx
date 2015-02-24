e3b95327943c09529f00d2126d337e9f7608e598
Max Claus Nunes <maxcnunes@gmail.com>
2015-02-24 09:48:45 -0300

    receives event objects with target and status through websockets

--- lib/monitor-app.jsx
+++ lib/monitor-app.jsx
@@ -6,9 +6,6 @@ var NavBar = require('./nav-bar.jsx');
 var Targets = require('./targets.jsx');
 
 module.exports = React.createClass({
-  componentDidMount: function() {
-    require('./websocket');
-  },
   render: function() {
     return <div className="monitor-app">
       <NavBar />
--- lib/targets.jsx
+++ lib/targets.jsx
@@ -1,27 +1,40 @@
+var _ = require('lodash');
 var React = require('react');
 var API = require('./api');
+var Websocket = require('./websocket');
 var api = new API();
+var websocket = new Websocket();
 
 require('./targets.scss');
 
 module.exports = React.createClass({
   getInitialState: function () {
+    websocket.onMessage(this.updateTarget);
     return { targets: [] };
   },
   componentDidMount: function() {
-    return api.all().then(function(result) {
+    api.all().then(function(result) {
       if (this.isMounted()) this.setState({ targets: result });
     }.bind(this));
   },
+  updateTarget: function (event) {
+    var target = _.find(this.state.targets, { url: event.target.url });
+    if (!target) {
+      target = event.target;
+      this.state.targets.push(target);
+    }
+    target.status = event.target.status;
+    this.setState({ targets: this.state.targets });
+  },
   render: function() {
-    var rows = this.state.targets.map(function (url) {
-      return <div>
-        <div className="up">
-          <h5><a href="#">{url}</a></h5>
-        </div>
-      </div>;
-    });
-
-    return <section id="targets">{rows}</section>;
+    return <section id="targets">{
+      this.state.targets.map(function (target) {
+        return <div key={target.url}>
+          <div className={target.status}>
+            <h5><a href="#">{target.url}</a></h5>
+          </div>
+        </div>;
+      })
+    }</section>;
   }
 });
